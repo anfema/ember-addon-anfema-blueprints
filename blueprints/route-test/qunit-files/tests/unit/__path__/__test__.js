baee7cca497fd7eae3289994165e39b3b4c850af
Florian Pichler <f.pichler@anfe.ma>
2017-02-15 15:45:20 +0100

    Copied blueprints from Ember.js repo

---
+++ blueprints/route-test/qunit-files/tests/unit/__path__/__test__.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:<%= dasherizedModuleName %>', '<%= friendlyTestDescription %>', {
+	// needs: ['controller:foo']
+});
+
+test('it exists', function (assert) {
+	const route = this.subject();
+
+	assert.ok(route);
+});
