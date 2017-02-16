84140025a7eb66d3f5047d1a760674a055b286b5
Florian Pichler <f.pichler@anfe.ma>
2017-02-16 09:43:57 +0100

    Added ember-data blueprints
    Re #1

--- blueprints/test-framework-detector.js
+++ blueprints/test-framework-detector.js
@@ -1,37 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const VersionChecker = require('ember-cli-version-checker');
+var fs = require('fs');
+var path = require('path');
+var VersionChecker = require('ember-cli-version-checker');
 
-module.exports = function (blueprint) {
-	blueprint.supportsAddon = function () {
-		return false;
-	};
+module.exports = function(blueprint) {
+  blueprint.supportsAddon = function() {
+    return false;
+  };
 
-	blueprint.filesPath = function () {
-		let type;
+  blueprint.filesPath = function() {
+    var type;
 
-		const dependencies = this.project.dependencies();
+    var dependencies = this.project.dependencies();
+    if ('ember-cli-qunit' in dependencies) {
+      type = 'qunit';
+    } else if ('ember-cli-mocha' in dependencies) {
+      var checker = new VersionChecker({ project: this.project });
+      if (fs.existsSync(this.path + '/mocha-0.12-files') && checker.for('ember-cli-mocha', 'npm').satisfies('>=0.12.0')) {
+        type = 'mocha-0.12';
+      } else {
+        type = 'mocha';
+      }
+    } else {
+      this.ui.writeLine('Couldn\'t determine test style - using QUnit');
+      type = 'qunit';
+    }
 
-		if ('ember-cli-qunit' in dependencies) {
-			type = 'qunit';
-		} else if ('ember-cli-mocha' in dependencies) {
-			const checker = new VersionChecker({ project: this.project });
+    return path.join(this.path, type + '-files');
+  };
 
-			const mochaFiles = fs.existsSync(`${this.path}/mocha-0.12-files`);
-
-			if (mochaFiles && checker.for('ember-cli-mocha', 'npm').satisfies('>=0.12.0')) {
-				type = 'mocha-0.12';
-			} else {
-				type = 'mocha';
-			}
-		} else {
-			this.ui.writeLine('Couldn\'t determine test style - using QUnit');
-
-			type = 'qunit';
-		}
-
-		return path.join(this.path, `${type}-files`);
-	};
-
-	return blueprint;
+  return blueprint;
 };
